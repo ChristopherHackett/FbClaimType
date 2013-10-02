d5e4ba20f796c7ef01348900ef8ddf65b8594344
Christopher Hackett <ChristopherHackett@users.noreply.github.com>
2013-10-02 21:37:38 +0100

    Now using 'chrome.i18n' for strings

--- plugin/background.js
+++ plugin/background.js
@@ -10,22 +10,22 @@ function onMessage(request, sender, sendResponse){
 			'priority': 100, 
 			'link': 'https://www.facebook.com/apps/application.php?id=',
 			'icon': icons.office, 
-			'text': 'Linked to application', 
-			'textLong': 'This webpage meta tag links it to a Facebook Application. This is the opticial way to link and control who has access to the Facebook insights. '
+			'text': chrome.i18n.getMessage("situationAppText"), 
+			'textLong': chrome.i18n.getMessage("situationAppTextLong")
 		}, 
 		'page':{
 			priority: 101, 
 			link: 'https://www.facebook.com/',
 			icon: icons.officeOld, 
-			text: 'Linked to page',
-			textLong: 'This webpage meta tag links it to a facebook page. This menthod is nolonger accpeted for new sites.'
+			'text': chrome.i18n.getMessage("situationPageText"), 
+			'textLong': chrome.i18n.getMessage("situationPageTextLong")
 		}, 
 		'admin':{
 			priority: 1, 
 			link: 'https://www.facebook.com/',
 			icon: icons.person, 
-			text: 'Linked to Facebook profile',
-			textLong: 'This webpage meta tag links it to a Facebook profile. This could identify the person with some associated with this site.'
+			'text': chrome.i18n.getMessage("situationAdminText"), 
+			'textLong': chrome.i18n.getMessage("situationAdminTextLong")
 		}
 	}
 	
